17e507e9c8ce013c660de3cee81ee15aaa99546f
Sandro Winkler <github@sandrowinkler.com>
2020-02-12 02:28:53 +0100

    Implemented make.sh

--- dotfiles/make.sh
+++ dotfiles/make.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 function install {
+  FILE="$1"
+  LINE="$2"
 
+  if [[ ! -f "$FILE" ]] || ! grep -Fxq "$LINE" "$FILE"; then
+    echo "$LINE" >> "$FILE" && echo "> Successfully added '$LINE' to '$FILE'"
+  else
+    echo "> '$LINE' was already added to '$FILE'"
+  fi
+    
 }
 
-if [ "$var1" = "foo" ]; then
-  echo "var1 == 'foo'"
-else
-  echo "var1 != 'foo'"
-fi
+
+echo "### INSTALLING DOTFILES by winklerrr ###"
+
+# get the current absolute directory
+DIR=$(cd $(dirname ${BASH_SOURCE}) >/dev/null 2>&1 && pwd)
+echo "> Found the files in '$DIR'"
+
+# install all scripts
+install "$HOME/.bashrc" "source $DIR/bashrc"
+install "$HOME/.vimrc" "source $DIR/vimrc"
+install "$HOME/.tmux.conf" "source $DIR/tmux.conf"
+install "$HOME/.inputrc" "\$include $DIR/inputrc"
+echo "> Done"
